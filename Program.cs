841bafa14e34eaf7ba89c834c609b8fd7a3621d9
Yung.Energy <morskojroma@gmail.com>
2022-06-30 19:38:15 +0300

    Task3

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите день недели(от 1 до 7): ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a >= 1 && a <= 5)
+{
+    Console.WriteLine($"{a} - нет");
+}
+else if (a == 6 || a == 7 )
+{
+    Console.WriteLine($"{a} - да");
+}
+else
+{
+    Console.WriteLine("Число должно быть от 1 до 7");
+}
+
+    
